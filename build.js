83ee8a4f35d65e611ad61f309652c1e045abdb09
Supratim Saha <74930882+Edward876@users.noreply.github.com>
2024-02-22 10:04:11 +0530

    Create build.js

---
+++ build.js
@@ -0,0 +1,10 @@
+import { exec } from 'child_process';
+
+exec('npx puppeteer browsers install chrome', (error, stdout, stderr) => {
+    if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+});
